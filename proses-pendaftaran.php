f92eef7eb256d25b39fe716656affa8e69074189
irchamali <id.irchamali@gmail.com>
2021-12-20 10:42:24 +0700

    Create delete data code on file hapus.php

--- proses-pendaftaran.php
+++ proses-pendaftaran.php
@@ -13,7 +13,7 @@ if (isset($_POST['daftar'])) {
     $sekolah = $_POST['sekolah_asal'];
 
     // buat query
-    $sql = "INSERT INTO calon_siswa (nama, alamat, jenis_kelamin, agama, sekolah_asal) VALUE ('$nama', '$alamat', '$jk', '$agama', '$sekolah')";
+    $sql = "INSERT INTO calon_mahasiswa (nama, alamat, jenis_kelamin, agama, sekolah_asal) VALUE ('$nama', '$alamat', '$jk', '$agama', '$sekolah')";
     $query = mysqli_query($db, $sql);
 
     // apakah query simpan berhasil?
