17e33167208e3d096bda4dbaa10b0b9ffdc3dbb2
irchamali <id.irchamali@gmail.com>
2021-12-20 09:13:14 +0700

    Create php files and make connection

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Pendaftaran Maba Unusia</title>
+</head>
+
+<body>
+    <header>
+        <h3>Pendaftaran Mahasiswa</h3>
+        <h1>UNUSIA Jakarta</h1>
+    </header>
+    <h4>Menu</h4>
+    <nav>
+        <ul>
+            <li><a href="form-daftar.php">Daftar Baru</a></li>
+            <li><a href="list-mahasiswa.php">Pendaftar</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
